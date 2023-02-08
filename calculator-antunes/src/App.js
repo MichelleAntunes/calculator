eb1cefcc8c83e07b58f514f159494168b7c1d3c7
MichelleAntunes <miichelleantuness@gmail.com>
2023-02-08 12:39:41 +0100

    Style initial with Tailwind

---
+++ calculator-antunes/src/App.js
@@ -0,0 +1,25 @@
+export default function App() {
+  return (
+    <div class="grid grid-cols-4 grid-rows-6 gap-4 p-5 justify-center items-center bg-black rounded-sm shadow-2xl shadow-orange-500/80 m-56 text-white w-80 h-80">
+      <p class="col-span-4 flex justify-center items-center">visor</p>
+      <p class="col-span-1 flex justify-center items-center">CE</p>
+      <p class="col-span-1 flex justify-center items-center">=></p>
+      <p class="col-span-1 flex justify-center items-center">,</p>
+      <p class="col-span-1 flex justify-center items-center">*</p>
+      <p class="col-span-1 flex justify-center items-center">7</p>
+      <p class="col-span-1 flex justify-center items-center">8</p>
+      <p class="col-span-1 flex justify-center items-center">9</p>
+      <p class="col-span-1 flex justify-center items-center">-</p>
+      <p class="col-span-1 flex justify-center items-center">4</p>
+      <p class="col-span-1 flex justify-center items-center">5</p>
+      <p class="col-span-1 flex justify-center items-center">6</p>
+      <p class="col-span-1 flex justify-center items-center">+</p>
+      <p class="col-span-1 flex justify-center items-center">1</p>
+      <p class="col-span-1 flex justify-center items-center">2</p>
+      <p class="col-span-1 flex justify-center items-center">3</p>
+      <p class="col-span-1 flex justify-center items-center">/</p>
+      <p class="col-span-1 flex justify-center items-center">0</p>
+      <p class="col-span-3 flex justify-center items-center">Enter</p>
+    </div>
+  );
+}
